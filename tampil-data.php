20839ad9600783a67ee93717c2d598f55c2898f2
Shindy <114824395+privatshin@users.noreply.github.com>
2023-04-26 00:05:27 +0700

    menambah data view

---
+++ tampil-data.php
@@ -0,0 +1,20 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$pass = "Dim88@4d3in"; // kalau kosong pakai ""
+$dbs  = "mahasiswa";
+
+$conn = new mysqli($host, $user, $pass, $dbs);
+
+if ($conn->connect_error) {
+    die("No Connection to Database");
+}
+
+$sql = "SELECT * FROM data_diri";
+
+if ($conn->query($sql)) {
+    $hasil = $conn->query($sql);
+
+    if($hasil->num_rows > 0) {
+?>
