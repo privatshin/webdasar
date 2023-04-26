b1c1b8e25bdeffa977861bf58733aaabcb0b16a4
Shindy <114824395+privatshin@users.noreply.github.com>
2023-04-26 20:14:44 +0700

    perubahan

--- tampil-data.php
+++ tampil-data.php
@@ -17,4 +17,30 @@ if ($conn->query($sql)) {
     $hasil = $conn->query($sql);
 
     if($hasil->num_rows > 0) {
-?>
+?> 
+        
+        
+        <table border="1" cellpadding="10" cellspacing="0">
+        <?php
+        while($row = $hasil->fetch_assoc()) {
+        ?>
+        <tr>
+        <?php
+            echo "<td>".$row['nim']."</td><td>"
+                 .$row['nama']."</td><td>"
+                 .$row['jenis_kelamin']."</td><td>"
+                 .$row['tpt_lahir']."</td><td>"
+                 .$row['tgl_lahir']."</td><td>"
+                 .$row['alamat']."</td>";
+        ?>
+        </tr>
+        <?php
+        }
+        ?>
+        </table>
+        <?php
+
+    }
+} else {
+    echo "perintah gagal dijalankan";
+}
